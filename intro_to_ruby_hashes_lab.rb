18185eb7b2ad3431432478cdb332d7571e9d91c4
Yevgeniy Sobolev <yevgeniyerot@gmail.com>
2019-02-23 02:02:57 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -64,7 +64,7 @@ monopoly = {
 	    pieces: 4,
 	    names: {
 	      reading_railroad: {
-	        
+	        "mortgage_value": "$100"
 	      },
 	      pennsylvania_railroad: {},
 	      b_and_o_railroad: {},
